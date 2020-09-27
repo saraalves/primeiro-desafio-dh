d1cef91799c5edfe09112b9b375f27468b626d81
Sara Alves <saraalves14@hotmail.com>
2020-09-27 15:02:45 -0300

    Desenvolvimento da classe Matricula

---
+++ src/Matricula.kt
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime
+import java.util.*
+
+class Matricula (val aluno: Aluno, val curso: Curso, var dataDaMatricula: LocalDateTime) {
+
+    init {
+        
+        dataDaMatricula = LocalDateTime.now()
+    }
+}

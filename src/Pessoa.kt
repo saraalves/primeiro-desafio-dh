60d59e106bd1ef1bafe3b25db1fc8698b5a9eb67
Sara Alves <saraalves14@hotmail.com>
2020-09-28 21:37:35 -0300

    Adicionado toString()

--- src/Pessoa.kt
+++ src/Pessoa.kt
@@ -1,3 +1,26 @@
 abstract class Pessoa (val nome: String, val sobrenome: String, val codigo: Int) {
 
+    override fun toString(): String {
+        return "Pessoa(nome='$nome', sobrenome='$sobrenome', codigo=$codigo)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Pessoa
+
+        if (nome != other.nome) return false
+        if (sobrenome != other.sobrenome) return false
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = nome.hashCode()
+        result = 31 * result + sobrenome.hashCode()
+        result = 31 * result + codigo
+        return result
+    }
 }

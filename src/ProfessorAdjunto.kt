22d142bb5132da1a6efac8c8e4435c5c8632bd2c
Sara Alves <saraalves14@hotmail.com>
2020-09-28 21:38:57 -0300

    adicionado hash e toString

--- src/ProfessorAdjunto.kt
+++ src/ProfessorAdjunto.kt
@@ -2,4 +2,22 @@ class ProfessorAdjunto (nome: String, sobrenome: String, codigoProfessor: Int, t
 
 ) : Professor(nome, sobrenome, codigoProfessor, tempoDeCasa,) {
 
+    override fun toString(): String {
+        return "ProfessorAdjunto(qtdHorasDeMonitoria=$qtdHorasDeMonitoria)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ProfessorAdjunto
+
+        if (qtdHorasDeMonitoria != other.qtdHorasDeMonitoria) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return qtdHorasDeMonitoria
+    }
 }
--- src/ProfessorTitular.kt
+++ src/ProfessorTitular.kt
@@ -1,4 +1,22 @@
 class ProfessorTitular (nome: String, sobrenome: String, codigoProfessor: Int, tempoDecasa: Int, val especialidade: String) :
     Professor(nome, sobrenome, codigoProfessor, tempoDecasa) {
 
+    override fun toString(): String {
+        return "ProfessorTitular(especialidade='$especialidade')"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ProfessorTitular
+
+        if (especialidade != other.especialidade) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return especialidade.hashCode()
+    }
 }

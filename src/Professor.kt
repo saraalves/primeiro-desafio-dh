ab262f8f483f5b533ce61c99a94b0479736190c9
Sara Alves <saraalves14@hotmail.com>
2020-09-28 21:38:24 -0300

    adicionado hash e toString

--- src/Professor.kt
+++ src/Professor.kt
@@ -1,3 +1,20 @@
-open class Professor (nome: String, sobrenome: String, codigoProfessor: Int, tempoDeCasa: Int) : Pessoa (nome, sobrenome, codigoProfessor){
+open class Professor (nome: String, sobrenome: String, codigoProfessor: Int, val tempoDeCasa: Int) : Pessoa (nome, sobrenome, codigoProfessor){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as Professor
+
+        if (tempoDeCasa != other.tempoDeCasa) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return tempoDeCasa
+    }
+
+    override fun toString(): String {
+        return "Professor(tempoDeCasa=$tempoDeCasa)"
+    }
 }
